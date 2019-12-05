db9e9b0a9b5be6e5ddfa479f9ed6ead83f75005b
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2019-12-05 17:02:13 -0300

    working

--- IA/RandomFlorest/main.py
+++ IA/RandomFlorest/main.py
@@ -12,7 +12,6 @@ from sklearn.metrics import accuracy_score
 
 def load_dataset(path : str = '', labels : [str] = ['A','B','C','D','E','F','L']) -> ([[int]],[str]):
   '''Loads dataset, returning a tuple with data, label and data paths'''
-  
   path = os.path.join(path,"Folds_Dataset_Final/*/")
   paths = {label:glob(f"{path}{label}/c*.PNG") for label in labels}
   data = {label:list(map(lambda path: load_img(path, color_mode='grayscale'), pathList)) for label, pathList in paths.items()}
@@ -25,5 +24,23 @@ def load_dataset(path : str = '', labels : [str] = ['A','B','C','D','E','F','L']
   # load dataset and check if it worked
 path = '../Data/'
 x, y, _ = load_dataset(path)
-display(x.shape)
-plt.imshow(X[80])
+plt.imshow(x[80])
+
+# reshape each image into a unidimensional matrix
+x = x.reshape(x.shape[0], x.shape[1]*x.shape[2])
+
+# shuffle data to make sure there is no underlying distribution
+x, y = shuffle(x, y)
+
+# split data into test and train sets
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# set random forest classifier
+model = RandomForestClassifier()
+
+# train model
+model.fit(x_train, y_train)
+
+# check results
+preds = model.predict(x_test)
+print("Accuracy:", accuracy_score(y_test, preds))
--- IA/install_depen.sh
+++ IA/install_depen.sh
@@ -1,9 +1,6 @@
- 
-```
 sudo pip3 install keras
 sudo pip3 install numpy
 sudo pip3 install sklearn
 sudo pip3 install itertools
 sudo pip3 install tensorflow
 sudo pip3 install matplotlib
-```
